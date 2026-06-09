1d29def1d6d9e778c11cb8a72427569459c8e2f6
thanlwinyeaung98@gmail.com <130201821+Thanlwin2062003@users.noreply.github.com>
2026-06-09 14:23:29 +0700

    Add Javascript lesson 3 files

---
+++ Javascript Lessons/lesson3.js
@@ -0,0 +1,34 @@
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+    console.log("The sky is blue!");
+} else {
+    console.log("The sky is not blue.");
+}
+
+let isRaining = false;
+
+if (isRaining) {
+    console.log("It's raining outside. Don't forget your umbrella!");
+}else {
+    console.log("It's not raining. Enjoy the sunshine!");
+}
+
+// if you see three lines. it's just three = in a row, ===. the font just combines them into one big character.
+if (2+2 === 4) {
+    console.log("oh thank god, the fundamentals princiles of mathematics still hold true"
+
+    );
+} else {
+    console.log("wait, what? 2+2 is not 4? Did I miss something in math class?");
+}
+
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 0) {
+  console.log("Cool, now I have a lot of nachos to myself.");
+} else if (friendsAtYourParty <= 4) {
+  console.log("Perfect amount to play some Mario Kart.");
+} else {
+  console.log("Wooooo turn on the dance music!");
+}
