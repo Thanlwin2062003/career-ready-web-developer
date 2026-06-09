fd65cf6cbd1fb68ae6d7c1cae1a8ea981de5f724
thanlwinyeaung98@gmail.com <130201821+Thanlwin2062003@users.noreply.github.com>
2026-06-09 15:07:43 +0700

    Add Javascript lesson4: HTML and JS files

---
+++ Javascript Lessons/lesson4.js
@@ -0,0 +1,24 @@
+let friendsAtYourParty = 0;
+while (friendsAtYourParty < 10) {
+    console.log(`You have ${friendsAtYourParty} friends at your party. Invite more!`);
+    friendsAtYourParty++;
+}
+console.log("Your party is now full! Time to celebrate!");
+
+let friendsAtYourParty2 = 0;
+while (friendsAtYourParty2 < 10) {
+    friendsAtYourParty2 = friendsAtYourParty2 + 1;
+}
+console.log(`You have ${friendsAtYourParty2} friends at your party. Time to celebrate!`);
+
+let friendsAtYourParty3 = 0;
+for (let i = 0; i <= 10; i++) {
+    friendsAtYourParty3++;
+}
+console.log(`You have ${friendsAtYourParty3} friends at your party. Time to celebrate!`);
+
+let friendsAtYourParty4 = 0;
+for (let i = 0; i < 10; i++) {
+    friendsAtYourParty4++;
+}
+console.log(`You have ${friendsAtYourParty4} friends at your party. Time to celebrate!`);
