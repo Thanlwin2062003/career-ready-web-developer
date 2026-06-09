d57ecd25287a129c33aa75da77b8856b6ed585ca
thanlwinyeaung98@gmail.com <130201821+Thanlwin2062003@users.noreply.github.com>
2026-06-09 13:41:58 +0700

    Update experiments.js and add lesson2 files

--- Javascript Lessons/experiments.js
+++ Javascript Lessons/experiments.js
@@ -1,3 +1,5 @@
 const monthlyRent = 500;
 const annualRent = monthlyRent * 12;
 console.log(annualRent); // Output: 6000
+
+console.log('don\'t do that');
---
+++ Javascript Lessons/lesson2.js
@@ -0,0 +1,10 @@
+const myName="Thanlwin";
+console.log(myName);
+
+const firstName = "Thanlwin";
+const lastName = "Aung";
+const sentence = "Hello, " + firstName + " " + lastName +"! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
