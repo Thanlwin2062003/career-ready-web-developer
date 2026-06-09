638212fdb2afc0d319ab07ff7c52e686f0a0ff0c
thanlwinyeaung98@gmail.com <130201821+Thanlwin2062003@users.noreply.github.com>
2026-06-09 15:55:57 +0700

    Update solution file

--- Javascript Lessons/solution1.js
+++ Javascript Lessons/solution1.js
@@ -1,9 +1,11 @@
 const timesToRepeat = 100;
 const character = "🐩";
 
-let word = ""; // start with an empty string
-for (let i = 0; i < timesToRepeat; i++) {
-  word = word + character;
-}
+// let word = ""; // start with an empty string
+// for (let i = 0; i < timesToRepeat; i++) {
+//   word = word + character;
+// }
 
-console.log(word);
+// console.log(word);
+
+console.log("".padStart(timesToRepeat, character));
