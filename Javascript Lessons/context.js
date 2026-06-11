e84120bf05c030d9ef449234ae95d975f72367cc
thanlwinyeaung98@gmail.com <130201821+Thanlwin2062003@users.noreply.github.com>
2026-06-11 16:22:38 +0700

    Add CSS lesson 1, practice files, and update JS context

---
+++ Javascript Lessons/context.js
@@ -0,0 +1,27 @@
+const me = {
+  name: {
+    first: "Brian",
+    last: "Holt",
+  },
+  location: {
+    streetNumber: 500,
+    street: "Fakestreet",
+    city: "Seattle",
+    state: "WA",
+    zipCode: 55555,
+    country: "USA",
+  },
+  getAddress() {
+    return `${this.name.first} ${this.name.last}
+${this.location.streetNumber} ${this.location.street}
+${this.location.city}, ${this.location.state} ${this.location.zipCode}
+${this.location.country}`;
+  },
+};
+
+console.log(me.getAddress());
+
+
+console.log(this === window);
+console.log(this.scrollY);
+console.log(window.scrollY);
